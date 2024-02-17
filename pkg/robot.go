ab21ef8ca6435c5de0c126aba5b5df695e11689a
Francisco Arrieta <61140677+wheredidfranciscogo@users.noreply.github.com>
2024-02-18 10:42:27 +1100

    add robot logic and testing

---
+++ pkg/robot.go
@@ -0,0 +1,41 @@
+package pkg
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+// Robot represents an assembled robot
+type Robot struct {
+	Components   []RobotComponent
+	SerialNumber string
+}
+
+// RobotComponent represents a component of a robot
+type RobotComponent struct {
+	Name string
+}
+
+// creates a new Robot instance with the given components
+func NewRobot(components []RobotComponent) *Robot {
+	return Robot{
+		Components:   components,
+		SerialNumber: generateSpecialNumber(),
+	}
+}
+
+// init initializes the random number generator
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+// generateSpecialNumber generates a special serial number for the robot
+func generateSpecialNumber() string {
+	const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	specialNumber := make([]byte, 6)
+	for i := range specialNumber {
+			specialNumber[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(specialNumber)
+}
